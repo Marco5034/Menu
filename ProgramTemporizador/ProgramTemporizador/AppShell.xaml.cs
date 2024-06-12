c6f587da830e073a1757e563fddd618ecf9c928c
vmarc <vmarc@DESKTOP-BNC8SKO>
2024-06-11 19:55:52 -0600

    # Se sube programa de Temporizador

---
+++ ProgramTemporizador/ProgramTemporizador/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgramTemporizador
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProgramTemporizador/ProgramTemporizador/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProgramTemporizador
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
